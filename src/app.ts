5a3e8e2f9eb868d135a563aa93422147519feab3
Quang Akiyama <quanganhcf2@gmail.com>
2021-11-12 11:55:18 +0700

    update homedir path

--- src/app.ts
+++ src/app.ts
@@ -62,7 +62,7 @@ class App {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(cookieParser());
-    this.app.use('/uploads', express.static('~/uploads'));
+    this.app.use('/uploads', express.static(path.join(homedir(), 'uploads')));
   }
 
   private initializeRoutes(routes: Routes[]) {
